93a1a41e61580c40f1258d8d88cadcb259b493cd
CodingCherry <cherry.alexeeva@gmail.com>
2020-04-20 17:21:12 +0400

    Initial Commit

---
+++ pixel-city/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pixel-city
+//
+//  Created by Alexeeva Ekaterina on 4/20/20.
+//  Copyright © 2020 Alexeeva Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
