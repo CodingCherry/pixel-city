cba34dce6e841e5a44ce85b90e5fe4dfb3a45877
CodingCherry <cherry.alexeeva@gmail.com>
2020-04-22 21:38:50 +0400

    Building MapVC UI

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
     }
 
+    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+    }
+    
+}
 
+extension MapVC: MKMapViewDelegate {
+    
 }
 
