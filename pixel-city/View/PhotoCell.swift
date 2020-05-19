dee8213d927a1ba83ad352ecda570c74c19603ce
CodingCherry <cherry.alexeeva@gmail.com>
2020-05-19 12:34:36 +0400

    Adding UILabel for Pull Up View

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Alexeeva Ekaterina on 5/19/20.
+//  Copyright © 2020 Alexeeva Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
